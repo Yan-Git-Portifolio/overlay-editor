d628f41aaccece3040ddc70d246c0e594966c346
Yan-Git-Portifolio <yanmiguel822@gmail.com>
2022-09-07 20:12:00 -0300

    Syntax Highlighter 1.0

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Text, Menu, Toplevel, LabelFrame, Label, Button
+from tkinter import Tk, Text, Menu, Toplevel, LabelFrame, Label, Button, IntVar
 import tkinter.font as tkfont
 from tkinter import ttk
 import json
@@ -33,8 +33,9 @@ class Main(Funcs):
         self.screen_configurator()
         self.text_configurator()
         self.menu_configurator()
+        self.screen.bind("<Key>", lambda event: self.update())
         self.screen.mainloop()
-
+        
     def screen_configurator(self):
         self.screen.title("Overlay Editor")
         self.screen.geometry(f"{WIDTH}x{HEIGHT}")
@@ -46,6 +47,16 @@ class Main(Funcs):
         font = tkfont.Font(font=self.code_text['font'])
         tab_size = font.measure('    ')
         self.code_text.config(tabs=tab_size)
+        self.code_text.tag_configure("orange", foreground = "orange", font=f"{atual['font_family']} {atual['font_size']}")
+        self.code_text.tag_configure("blue", foreground = "blue", font=f"{atual['font_family']} {atual['font_size']}")
+        self.code_text.tag_configure("purple", foreground = "purple", font=f"{atual['font_family']} {atual['font_size']}")
+        self.code_text.tag_configure("green", foreground = "green", font=f"{atual['font_family']} {atual['font_size']}")
+        self.code_text.tag_configure("red", foreground = "red", font=f"{atual['font_family']} {atual['font_size']}")
+        self.tags = ["orange", "blue", "purple", "green", "red"]
+        self.wordlist = [ ["class", "def", "for", "if", "else", "elif", "import", "from", "as", "break", "while"],
+                          ["int", "string", "float", "bool", "__init__"],
+                          ["pygame", "tkinter", "sys", "os", "mysql"],
+                          ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"] ]
         self.code_text.pack(fill="both", expand=True)
     def menu_configurator(self):
         self.main_menu = Menu(self.screen)
@@ -90,9 +101,55 @@ class Main(Funcs):
         fontes.set(atual["font_family"])
         fontes.pack(pady=5)
         aplicar = Button(preferences_screen, text="Aplicar", bg="#ddd",
-                         command=lambda : self.aplicar_preferencias(tema=temas.get(), tamanho=int(tamanho.get()), familia=fontes.get()))
+                         command=lambda: self.aplicar_preferencias(tema=temas.get(), tamanho=int(tamanho.get()), familia=fontes.get()))
         aplicar.pack(pady=7)
 
+    def tagHighlight(self):
+        start = "1.0"
+        end = "end"
+         
+        for mylist in self.wordlist:
+            num = int(self.wordlist.index(mylist))
+ 
+            for word in mylist:
+                self.code_text.mark_set("matchStart", start)
+                self.code_text.mark_set("matchEnd", start)
+                self.code_text.mark_set("SearchLimit", end)
+ 
+                mycount = IntVar()
+                 
+                while True:
+                    index= self.code_text.search(word,"matchEnd","SearchLimit", count=mycount, regexp = False)
+ 
+                    if index == "": break
+                    if mycount.get() == 0: break
+ 
+                    self.code_text.mark_set("matchStart", index)
+                    self.code_text.mark_set("matchEnd", "%s+%sc" % (index, mycount.get()))
+ 
+                    preIndex = "%s-%sc" % (index, 1)
+                    postIndex = "%s+%sc" % (index, mycount.get())
+                     
+                    if self.check(index, preIndex, postIndex):
+                        self.code_text.tag_add(self.tags[num], "matchStart", "matchEnd")
+                    
+    def check(self, index, pre, post):
+        letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+                   "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+ 
+        if self.code_text.get(pre) == self.code_text.get(index):
+            pre = index
+        else:
+            if self.code_text.get(pre) in letters:
+                return 0
+ 
+        if self.code_text.get(post) in letters:
+            return 0
+ 
+        return 1
+                         
+    def update(self):
+        self.tagHighlight()
 
 if __name__ == "__main__":
     Main()
